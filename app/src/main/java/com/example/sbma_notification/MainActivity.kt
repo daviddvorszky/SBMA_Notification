21f66fe3cf9762f655217af3e75f1546bd40c126
xR3l4Xx <halloka.mangoka@gmail.com>
2022-08-31 14:58:55 +0300

    Add notifications

--- app/src/main/java/com/example/sbma_notification/MainActivity.kt
+++ app/src/main/java/com/example/sbma_notification/MainActivity.kt
@@ -1,18 +1,20 @@
 package com.example.sbma_notification
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.example.sbma_notification.ui.theme.SBMA_NotificationTheme
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
@@ -41,6 +43,13 @@ fun NotificationMain() {
     var prevColor by remember { mutableStateOf(randomColor())}
     var currentColor by remember { mutableStateOf(randomColor())}
 
+    val context = LocalContext.current
+    val channelId = "SBMA_NotifChannel"
+    val notificationId = 0
+    
+    LaunchedEffect(Unit){
+        createNotificationChannel(channelId, context)
+    }
 
     Scaffold(
         scaffoldState = scaffoldState,
@@ -56,23 +65,86 @@ fun NotificationMain() {
         backgroundColor = currentColor,
         modifier = Modifier.fillMaxSize()
     ){
-        Button(onClick = {
-            coroutineScope.launch {
-                prevColor = currentColor
-                currentColor = randomColor()
-                val snackbarResult = scaffoldState.snackbarHostState.showSnackbar(
-                    message = "Color changed",
-                    actionLabel = "Undo"
-                )
-                when (snackbarResult) {
-                    SnackbarResult.ActionPerformed -> {
-                        currentColor = prevColor
+        Column {
+            Button(
+                onClick = {
+                    coroutineScope.launch {
+                        prevColor = currentColor
+                        currentColor = randomColor()
+                        val snackbarResult = scaffoldState.snackbarHostState.showSnackbar(
+                            message = "Color changed",
+                            actionLabel = "Undo"
+                        )
+                        when (snackbarResult) {
+                            SnackbarResult.ActionPerformed -> {
+                                currentColor = prevColor
+                            }
+                            SnackbarResult.Dismissed -> {}
+                        }
                     }
-                    SnackbarResult.Dismissed -> {}
-                }
+                },
+            ) {
+                Text("Change Color!")
             }
-        }){
-            Text("Change Color!")
+
+            Button(onClick = {
+                showNotification(
+                    context,
+                    channelId,
+                    notificationId,
+                    "Simple Notification",
+                    "This is a simple notification with default priority"
+                )
+            }) {
+                Text("Simple Notification")
+            }
+
+            Button(onClick = {
+                showNotification(
+                    context,
+                    channelId,
+                    notificationId,
+                    "HIGH PRIORITY NOTIFICATION",
+                    "This is a simple notification with high priority",
+                    NotificationCompat.PRIORITY_HIGH
+                )
+            }) {
+                Text("High Priority Notification")
+            }
         }
+    }
+}
+
+fun createNotificationChannel(channelId: String, context: Context){
+    //if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+        val name = "SBMA Notification Channel"
+        val descriptionText = "Assignment test channel"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(channelId, name, importance).apply {
+            description = descriptionText
+        }
+
+        val notificationManager: NotificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+    //}
+}
+
+fun showNotification(
+    context: Context,
+    channelId: String,
+    notificationId: Int,
+    textTitle: String,
+    textContent: String,
+    priority: Int = NotificationCompat.PRIORITY_DEFAULT
+){
+    val builder = NotificationCompat.Builder(context, channelId)
+        .setSmallIcon(R.drawable.ic_launcher_foreground)
+        .setContentTitle(textTitle)
+        .setContentText(textContent)
+        .setPriority(priority)
+
+    with(NotificationManagerCompat.from(context)){
+        notify(notificationId, builder.build())
     }
 }
